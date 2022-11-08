45e9d8ddf3380936a98b29245d705f76ddaad5c3
aaaustralias <oscarjdevs@gmail.com>
2022-11-08 23:42:26 +1100

    Clicker working perfectly

--- clicker/main.js
+++ clicker/main.js
@@ -1,6 +1,7 @@
-var data = {cookies: 0, increment: 1};
+var data = {cookies: 0, increment: 1, autoclickers: {}};
 
-var autoclickers = [{name: "Bacteria-I", increment: 1, cost: 100}, {name: "Bacteria-II", increment: 10, cost: 1000}]
+var autoclickers = [{name: "Bacteria-I", increment: 182475528479, cost: 10}, {name: "Bacteria-II", increment: 10, cost: 250}, {name: "Bacteria-III", increment: 10000, cost: 1000}]
+var autoclickerconvert = {}
 
 /*function myfunction() {
 	data.cookies += 100;
@@ -14,23 +15,41 @@ var autoclickers = [{name: "Bacteria-I", increment: 1, cost: 100}, {name: "Bacte
 	}
 }*/
 
+setInterval(function(){ runautoclickers(); }, 50);
+
 setTimeout(load, 1000);
 
 function load() {
-	data.cookies += 100;
-	let 
 	for (let x in autoclickers) {
+		autoclickerconvert[autoclickers[x].name] = x;
 		var div = document.createElement("div");
-		div.innerText = autoclickers[x].name;
-		document.body.appendChild(div);
+		div.classList.add(autoclickers[x].name);
+		div.onclick = function() { purchaseautoclicker(x); }
+		document.querySelector(".autoclicker-content").appendChild(div);
+		var selector = document.querySelector(`.${autoclickers[x].name}`)
+		var el = document.createElement("h2"); el.textContent = autoclickers[x].name; selector.appendChild(el);
+		var el = document.createElement("p"); el.textContent = `Cost: ${autoclickers[x].cost} - CPS: +${autoclickers[x].increment}`; selector.appendChild(el);
 	}
-	clickcookie();
+	selectpanel("autoclicker")
+}
+
+function runautoclickers() {
+	let count = 0
+	for (let x in data.autoclickers) {
+		count += autoclickers[x].increment * data.autoclickers[String(x)];
+	}
+	data.cookies += (count / 20);
+	updatecookie();
 }
 
 function clickcookie() {
-  data.cookies += data.increment;
-  document.querySelector(".count").textContent = data.cookies.toLocaleString();
+	data.cookies += data.increment;
 	document.querySelector(".cookie").style.animation = "animation: click 200ms";
+	updatecookie();
+}
+
+function updatecookie() {
+	document.querySelector(".count").textContent = Math.round(data.cookies).toLocaleString();
 }
 
 function upgradecookie() {
@@ -41,4 +60,18 @@ function selectpanel(panel) {
 	document.querySelector(".autoclicker-content").style.display = "none";
 	document.querySelector(".upgrade-content").style.display = "none";
 	document.querySelector(`.${panel}-content`).style.display = "flex";
+	document.querySelector(".autoclicker-panel").style.backdropFilter = "none";
+	document.querySelector(".upgrade-panel").style.backdropFilter = "none";
+	document.querySelector(`.${panel}-panel`).style.backdropFilter = "brightness(5%)";
+}
+
+function purchaseautoclicker(ac) {
+	var autoclicker = autoclickers[ac];
+	if (data.cookies >= autoclicker.cost) {
+		(!(ac in data.autoclickers)) ? data.autoclickers[ac] = 1 : data.autoclickers[ac] ++;
+		data.cookies -= autoclicker.cost
+		updatecookie();
+	} else {
+		alert("Can't afford!");
+	}
 }
